c63e70b922b9dbd40e96994203071278081cb5ed
Yahnis Saint-Val <yahnis.saint-val@etu.umontpellier.fr>
2024-10-16 18:55:07 +0200

    Fetch job info from vjk <3

--- JobAlert/main.py
+++ JobAlert/main.py
@@ -6,26 +6,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
+job_description_class = "fastviewjob jobsearch-ViewJobLayout--embedded css-1s5gqtr eu4oa1w0 hydrated"
+
+def getSoupFromURL(url,delay=2):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
+    time.sleep(delay)
+    # Récupérer le code HTML de la page
+    page_source = driver.page_source
+    # Fermer le navigateur une fois le scraping terminé
+    driver.quit()
+    # Parse le contenu HTML
+    return BeautifulSoup(page_source, 'html.parser')
+
 # URL de la page web
-url = 'https://fr.indeed.com/jobs?q=analyse+d%27image&l=Lyon+%2869%29&from=searchOnDesktopSerp&vjk=c2b6128c049d8b48'
+url = 'https://fr.indeed.com/jobs?q=analyse+d%27image&l=Lyon+%2869%29&from=searchOnDesktopSerp'
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)  # Ouvre le navigateur et charge la page
-
-# Optionnel : Attendre quelques secondes pour que la page charge
-time.sleep(5)
-
-# Récupérer le code HTML de la page
-page_source = driver.page_source
-# Fermer le navigateur une fois le scraping terminé
-driver.quit()
-
-# Parse le contenu HTML
-soup = BeautifulSoup(page_source, 'html.parser')
-
-with open('testext.txt', 'w', encoding='utf-8') as file:
-    # Parcourir les titres et les enregistrer dans le fichier
-    file.write(soup.get_text())
+soup = getSoupFromURL(url)
 
 # Trouver toutes les balises contenant l'identifiant unique 'vjk'
 # En général, les identifiants vjk se trouvent dans des balises <a> avec des attributs data-jk ou similaires
@@ -37,3 +34,12 @@ vjk_ids = [job['data-jk'] for job in job_elements]
 # Afficher les identifiants extraits
 for vjk_id in vjk_ids:
     print(vjk_id)
+
+url_1 = url + "&vjk=" + vjk_ids[0]
+
+soup = getSoupFromURL(url_1)
+
+with open('testext.txt', 'w', encoding='utf-8') as file:
+    # Parcourir les titres et les enregistrer dans le fichier
+    jobdescription_div = soup.find('div', class_=job_description_class)
+    file.write(jobdescription_div.get_text())
